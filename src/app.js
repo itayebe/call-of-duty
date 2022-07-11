38bd798da6e7275505684a7641dc8cb893133a35
IE783546 <IE783546@greendreamteam.onmicrosoft.com>
2022-07-11 13:15:48 +0300

    Replace express with fastify

--- src/app.js
+++ src/app.js
@@ -1,14 +1,19 @@
-import express from 'express';
+import Fastify from 'fastify'
 import 'dotenv/config';
 import { addSoldier } from './mongodb.js';
 
-export const app = express();
-
-app.get('/health', (req, res) => {
-  res.status(200).send('health');
+export const app = Fastify({
+  logger: false
 });
 
-app.post('/soldiers', (req, res) => {
-  addSoldier(req.body);
-  res.status(201);
-});
+app.get('/health', (request, reply) => {
+  reply.status(200).send();
+})
+
+// app.post('/p' , (req,res) => {
+//   res.send(req.body);
+// })
+
+// app.post('/soldiers', (req, res) => {
+//   res.status(201).send('sol');
+// });
--- src/server.js
+++ src/server.js
@@ -2,6 +2,12 @@ import {app} from './app.js';
 
 const port = process.env.PORT || '3000';
 
-app.listen(port, () => {
-  console.log('listening');
+app.listen({ port }, (err, address) => {
+  // if (err) {
+  //   app.log.error(err)
+  //   process.exit(1)
+  // }
+  // else{
+    console.log(`listening to ${port}`);
+  // }
 });
